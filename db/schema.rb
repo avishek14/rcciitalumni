d5a86cb3340499b644a0a7e170034ddd91250c7d
Kaylors <shuvophoenix@gmail.com>
2014-01-29 11:58:29 +0530

    Changed the database from pg to sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140110130503) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "dailies", force: true do |t|
     t.string   "title"
     t.datetime "date"
